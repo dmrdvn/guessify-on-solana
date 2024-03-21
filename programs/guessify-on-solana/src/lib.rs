96549cc6d983af03639f1decf0b208b58151e453
dmrdvn <demirdoven.hh@gmail.com>
2024-03-21 21:34:21 +0300

    Application flow structure has become stable

--- programs/guessify-on-solana/src/lib.rs
+++ programs/guessify-on-solana/src/lib.rs
@@ -1,118 +1,130 @@
 use anchor_lang::prelude::*;
-use anchor_lang::system_program;
-
-// This is your program's public key and it will update
-// automatically when you build the project.
 declare_id!("E8izq3XUK9HGB4NQkz8n6mDvQTBoNTW3yg8Ft1XwxBuV");
 
 #[program]
-mod guessify {
+pub mod guessify_on_solana {
     use super::*;
 
-   
-
-    pub fn create_user(ctx: Context<CreateUser>, user_wallet: Pubkey) -> Result<()> {
-        let user = &mut ctx.accounts.user;
-        user.wallet = user_wallet; // Kullanıcı hesabı anahtarını kullanıcı cüzdanı anahtarı olarak ayarla
-        println!("User created: {:?}", user.wallet);
+    pub fn create_user(ctx: Context<CreateUser>, user_pubkey: Pubkey) -> Result<()> {
+        let user_account = &mut ctx.accounts.user_account;
+        user_account.user_pubkey = user_pubkey;
         Ok(())
     }
 
-    pub fn create_post(ctx: Context<CreatePost>, content: String) -> Result<()> {
-        let post = &mut ctx.accounts.post;
-        let user = &mut ctx.accounts.user;
-        let author_key = *ctx.accounts.user.to_account_info().key;
-        post.author = author_key;
-        post.content = content;
-        post.finished = true;
+    pub fn create_post(
+        ctx: Context<CreatePost>,
+        content: String,
+        bet_amount: u64,
+        due_date: i64,
+    ) -> Result<()> {
+        let post_account = &mut ctx.accounts.post_account;
+        post_account.author = *ctx.accounts.author.key;
+        post_account.content = content;
+        post_account.bet_amount = bet_amount;
+        post_account.due_date = due_date;
+        post_account.finished = false;
 
         let clock = Clock::get().unwrap();
-        post.timestamp = clock.unix_timestamp;
+        post_account.publish_date = clock.unix_timestamp;
+
+        let participation_account = &mut ctx.accounts.participation_account;
+        participation_account.user_pubkey = *ctx.accounts.author.to_account_info().key;
+        participation_account.post_pubkey = *ctx.accounts.post_account.to_account_info().key;
+        participation_account.bet_amount = bet_amount;
+        participation_account.vote = true;
 
         Ok(())
     }
 
-    pub fn participate(ctx: Context<Participate>, post_public_key: Pubkey) -> Result<()> {
-        let participant_to_post = &mut ctx.accounts.participant_to_post; //bir participant account oluştur
-        participant_to_post.participant = *ctx.accounts.participant.to_account_info().key;
-        participant_to_post.post_pubkey = post_public_key; // Post public key'i parametre olarak alınıyor
-
+    pub fn participate_post(
+        ctx: Context<ParticipatePost>,
+        bet_amount: u64,
+        vote: bool,
+    ) -> Result<()> {
+        let participation_account = &mut ctx.accounts.participation_account;
+        participation_account.user_pubkey = *ctx.accounts.participant.to_account_info().key;
+        participation_account.post_pubkey = *ctx.accounts.post.to_account_info().key;
+        participation_account.bet_amount = bet_amount;
+        participation_account.vote = vote;
         Ok(())
     }
-
-
-
-
 
 }
-///////////////////////////////////////////////////////////////
-////////////////////// USER ///////////////////////////////////
-///////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////////
+///////////////////////////// USER //////////////////////////////////
+/////////////////////////////////////////////////////////////////////
+
 #[derive(Accounts)]
 pub struct CreateUser<'info> {
+    #[account(init, payer = user, space = 8 + 32)]
+    pub user_account: Account<'info, User>,
     #[account(mut)]
-    pub signer: Signer<'info>,
-
-    #[account(init, payer = signer, space = 1000)]
-    pub user: Account<'info, User>,
-    pub system_program: AccountInfo<'info>,
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
 }
 
 #[account]
 pub struct User {
-    wallet: Pubkey,
-    
+    pub user_pubkey: Pubkey,
 }
 
-
-
 //////////////////////////////////////////////////////////////////////
-////////////////////// CREATE POST  //////////////////////////////////
+///////////////////////////// POST  //////////////////////////////////
 //////////////////////////////////////////////////////////////////////
+
 #[derive(Accounts)]
 pub struct CreatePost<'info> {
+    #[account(init, payer = author, space = 2000)]
+    pub post_account: Account<'info, Post>,
     #[account(mut)]
-    pub signer: Signer<'info>,
+    pub author: Signer<'info>,
 
-    #[account(init, payer = signer, space = 1000)]
-    pub post: Account<'info, Post>,
-
-    #[account(mut)]
-    pub user: Account<'info, User>, // Kullanıcı hesabını burada tanımlayın
-    
-    pub system_program: AccountInfo<'info>,
+    #[account(init, payer = author, space = 2000)]
+    pub participation_account: Account<'info, Participation>,
+    pub system_program: Program<'info, System>,
 }
 
 #[account]
 pub struct Post {
     pub author: Pubkey,
     pub content: String,
-    pub timestamp: i64,
+    pub bet_amount: u64,
+    pub due_date: i64,
+    pub publish_date: i64,
     pub finished: bool,
 }
 
-
 //////////////////////////////////////////////////////////////////////
-///////////////// PARTICIPATE POST  //////////////////////////////////
+/////////////////////// PARTICIPATION  ///////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
 #[derive(Accounts)]
-pub struct Participate<'info> {
+pub struct ParticipatePost<'info> {
+    #[account(init, payer = participant, space = 2000)]
+    pub participation_account: Account<'info, Participation>,
     #[account(mut)]
-    pub participant: Signer<'info>, //Ödeyen
-
+    pub participant: Signer<'info>,
+    
     #[account(mut)]
-    pub post: Account<'info, Post>, //Post
-    pub user: Account<'info, User>, //Katılımcı
-
-    #[account(init, payer = participant, space = 1000)]
-    pub participant_to_post: Account<'info, ParticipantToPost>,
-    pub system_program: AccountInfo<'info>,
+    pub post: Account<'info, Post>,
+    pub system_program: Program<'info, System>,
 }
 
 #[account]
-pub struct ParticipantToPost {
-    participant: Pubkey, //katılımcının pubkeyi
-    post_pubkey: Pubkey, //postun pubkeyi
-    /* bet_amount: u64,   */     //bet amount
+pub struct Participation {
+    pub user_pubkey: Pubkey, 
+    pub post_pubkey: Pubkey,
+    pub bet_amount: u64,
+    pub vote: bool,
+}
+
+/////////////////////////////////////////////////////////////////////
+/////////////////////////// ERRORS //////////////////////////////////
+/////////////////////////////////////////////////////////////////////
+
+#[error_code]
+pub enum CustomError {
+    #[msg("The specified post does not exist.")]
+    PostNotFound,
 }
